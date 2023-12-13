a79802cccdbabe4e53b623bf17bdb888c099d912
Albert Fit <albert.ioan.fit@gmail.com>
2023-12-13 08:36:31 +0000

    Importing Multiple Classes from a Module

---
+++ my_electric_car.py
@@ -0,0 +1,7 @@
+from car import ElectricCar
+
+my_leaf = ElectricCar('nissan', 'leaf', 2024)
+print(my_leaf.get_descriptive_name())
+my_leaf.battery.describe_battery()
+my_leaf.fill_gas_tank()
+my_leaf.battery.get_range()
